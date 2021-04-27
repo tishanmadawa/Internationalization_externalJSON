bda77b76843ea2b690571a0c41f6798b994ddd74
tishank <tishank@millenniumitesp.com>
2021-04-27 18:24:11 +0530

    initial code

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+import { useTranslation } from 'react-i18next';
+import './i18n';
+
+//npm install react-i18next i18next --save
+const App = () => {
+  const { t, i18n } = useTranslation();
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          {t("title")}
+        </p>
+        <button onClick={() => {
+          i18n.changeLanguage('si-LK');
+        }}>
+          Sinhala
+        </button>
+        <button onClick={() => {
+          i18n.changeLanguage('en-US');
+        }}>
+          English
+        </button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/i18n.js
@@ -0,0 +1,57 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+// the translations
+// (tip move them in a JSON file and import them)
+var resources = {};
+
+fetch("https://sqlvajn5qbt7ya7yq4.blob.core.windows.net/feedback-file/en-US.json")
+    .then(data => data.json())
+    .then(data => {
+        resources["en-US"] = data
+        i18n
+            .use(initReactI18next) // passes i18n down to react-i18next
+            .init({
+                resources,
+                lng: "en-US",
+
+                keySeparator: false, // we do not use keys in form messages.welcome
+
+                interpolation: {
+                    escapeValue: false // react already safes from xss
+                }
+            });
+
+    })
+
+fetch("https://sqlvajn5qbt7ya7yq4.blob.core.windows.net/feedback-file/si-LK.json")
+    .then(data => data.json())
+    .then(data => {
+        resources["si-LK"] = data
+        i18n
+            .use(initReactI18next) // passes i18n down to react-i18next
+            .init({
+                resources,
+                lng: "en-US",
+
+                keySeparator: false, // we do not use keys in form messages.welcome
+
+                interpolation: {
+                    escapeValue: false // react already safes from xss
+                }
+            });
+    })
+
+i18n
+    .use(initReactI18next) // passes i18n down to react-i18next
+    .init({
+        resources,
+        lng: "en-US",
+
+        keySeparator: false, // we do not use keys in form messages.welcome
+
+        interpolation: {
+            escapeValue: false // react already safes from xss
+        }
+    });
+
+export default i18n;
